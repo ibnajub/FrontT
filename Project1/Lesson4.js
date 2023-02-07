e974b6c0467c762dc38f42f37057fa3d65619300
Artem <ibnajub@gmail.com>
2023-02-08 01:42:50 +0200

    final4

--- Project1/Lesson4.js
+++ Project1/Lesson4.js
@@ -162,81 +162,89 @@ console.log(func.call(objectA))
 // };
 // const result = getNewArray...; // Ссылаясь на массив ключа values из valObject0 [4, 8]
 console.log("Блок 2 Задание 3");
-
-
+function getNewArray23() {
+    if (this.values === undefined) {
+        return 'Не найдено'
+    }
+    return this.values.filter((curVal) => (typeof curVal === 'number' && curVal > 2 && curVal < 10 && curVal % 2 === 0));
+};
+const valObject0 = {
+    values: [1, '2', 4, 8, '8', 3, 10, null, false],
+};
+console.log(getNewArray23.call(valObject0))
 
 // Блок 3
 
 //     Дан объект с городами и странами.
-
 // Написать функцию getCity. Эта функция (getCity) должна вернуть новый массив, элементы которого будут преобразованы в данный формат: <Столица> - это <Страна>.
-
 // Доступ к объекту может быть любым (через контекст, напрямую и т.д.)
-
 // Можно использовать Object.entries метод )
+console.log("Блок 3 Задание 1");
 
-// const citiesAndCountries = {
-// 	'Киев': 'Украина',
-// 	'Нью-Йорк': 'США',
-// 	'Амстердам': 'Нидерланды',
-// 	'Берлин': 'Германия',
-// 	'Париж': 'Франция',
-// 	'Лиссабон': 'Португалия',
-// 	'Вена': 'Австрия',
-// };
-// const result = getCity(); // ['Киев - это Украина', 'Нью-Йорк - это США', ... и т.д.]
+const citiesAndCountries = {
+    'Киев': 'Украина',
+    'Нью-Йорк': 'США',
+    'Амстердам': 'Нидерланды',
+    'Берлин': 'Германия',
+    'Париж': 'Франция',
+    'Лиссабон': 'Португалия',
+    'Вена': 'Австрия',
+};
+function getCity() {
+    return Object.entries(citiesAndCountries).map(([city, country]) => { return `${city} - это ${country}` })
+}
+console.log(getCity()); // ['Киев - это Украина', 'Нью-Йорк - это США', ... и т.д.]
+
 
 //     Создать объект с названиями дней недели. Где ключами будут ru и en, a значением свойства ru будет массив с названиями дней недели на русском, а en - на английском.
-
 // После написать функцию которая будет выводить в консоль название дня недели пользуясь выше созданным объектом (доступ к объекту можно получить напрямую).
-
 // Все дни недели начинаются с 1 и заканчиваются цифрой 7 (1- понедельник, 7 - воскресенье).
-
 // Функция принимает в аргументы 2 параметра:
 //     lang - название языка дня недели
 //     day - число дня недели
-
 // Можно вспомнить про метод indexOf(). А может можно и без него :)
-// const namesOfDays = {
-//     ru: ['Понедельник', 'Вторник', 'Среда', ... , 'Воскресенье'],
-//     en: ['Monday', 'Tuesday', 'Wednesday', ... , 'Sunday'],
-// }
-// ------------------------------------------------
-// // Пример 1
-// function getNameOfDay(lang, datNumber){
-//     ... Your code
-// }
-// getNameOfDay('en', 7) // 'Sunday'
-// ------------------------------------------------
-// // Пример 2
-// function getNameOfDay(lang, datNumber){
 
-//     ... Your code
-// }
-// getNameOfDay('ru', 3) // 'Среда'
+console.log("Блок 3 Задание 2");
+const namesOfDays = {
+    "ru": ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    "en": ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+}
+// ------------------------------------------------
+//  Пример 1
+function getNameOfDay(lang, datNumber) {
+    return namesOfDays[lang][datNumber - 1]
+}
+console.log(getNameOfDay('en', 7));// 'Sunday'
+console.log(getNameOfDay('ru', 3)); // 'Среда'
 
-//     Написать универсальную функцию setProto, которая принимает в себя 2 аргумента (currentObj, protoObj).
+
+
+// Написать универсальную функцию setProto, которая принимает в себя 2 аргумента (currentObj, protoObj).
 //Функция должна устанавливать прототип (protoObj) для currentObj. То есть после вызова функции мы должны получить результат:
-// const person = {
-//     name: 'Vlad'
-// };
-// const person1 = {
-//     age: 1
-// };
-// function setProto (currentObj, protoObj) {
-//     // code
-// }
-// setProto(person1, person);
-// // Теперь прототипом для объекта person1 выступает объект person
+console.log("Блок 3 Задание 3");
 
-//     Создать базовый объект person. Этот объект должен выступать в роли прототипа для объекта person1.
+const person0 = {
+    name: 'Vlad'
+};
+const person11 = {
+    age: 1
+};
 
+function setProto(currentObj, protoObj) {
+    currentObj.__proto__ = protoObj
+}
+// Теперь прототипом для объекта person1 выступает объект person
+setProto(person11, person0);
+console.log(person11);
+
+
+
+
+// Создать базовый объект person. Этот объект должен выступать в роли прототипа для объекта person1.
 // В объекте person должны быть такие методы:
-
 //     метод для установки имени и возраста (setName, setAge)
 //     метод для получения имени и возраста (getName, getAge)
 //     метод для валидации возраста (ageValidation)
-
 // person1.setName(...); // установили новое имя
 // person1.getName(); // имя
 // person1.setAge(...); // установили возраст
@@ -244,10 +252,32 @@ console.log("Блок 2 Задание 3");
 
 // Метод ageValidation вызывается при вывозе метода setAge (то есть внутри метода setAge). Метод ageValidation должен как-то проверить возраст,
 // который мы вводим в setAge. Если возраст, который мы ввели, меньше 18, то записываем в age слово 'Validation Error', а есть введенный возраст больше 18, то вписываем в age это значение.
-
 // ageValidation только проверяет данные, он ничего не записывает (в ageValidation не должно быть this.age = age)
 
-// person1.setAge(1); // передать возраст можно как угодно
-// person1.getAge(); // 'Validation Error'
-// person1.setAge(20); // передать возраст можно как угодно
-// person1.getAge(); // Новое значение - 20
+console.log("Блок 3 Задание 4");
+
+const person = {
+    name: 'Vlad'
+};
+const person1 = {
+    a: 1,
+    setName(arg) { this.name = arg },
+    "getName": function () { return this.name },
+    setAge(arg) {
+        if (this.ageValidation(arg)) { this.age = arg }
+        else { this.age = 'Validation Error' }
+    },
+    getAge() { return this.age },
+
+    ageValidation(age) { return age > 18 },
+
+};
+
+person1.__proto__ = person;
+
+person1.setName('Igor');
+console.log(person1.getName());
+person1.setAge(1); // передать возраст можно как угодно
+console.log(person1.getAge()); // 'Validation Error'
+person1.setAge(20); // передать возраст можно как угодно
+console.log(person1.getAge()); // Новое значение - 20
